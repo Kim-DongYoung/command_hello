d7cd12fa89dbce9043080ac876323f539dedc230
tearang <tearang3@gmail.com>
2018-08-29 13:46:10 +0900

    remote repository "Third : Add python File Push"

---
+++ pushprint.py
@@ -0,0 +1,13 @@
+# For command line git tutorial_KDY
+# First : GitHub modification
+# Second : Local repository modification
+# Third : Add python File Push
+
+
+
+print(a, "Hello world")
+print(b, "Tell your world")
+print(c1, "Tell his world")
+print(c2, "Tell her world")
+print("Tell my world")
+print("Here is Eclipse")
